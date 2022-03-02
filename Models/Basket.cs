ae8a079df771b9a69a0ff42637f836bbc6cc7d1a
Jane Lee <xo930497@yahoo.com.tw>
2022-03-02 15:50:57 -0700

    initial

--- Models/Basket.cs
+++ Models/Basket.cs
@@ -9,7 +9,7 @@ namespace Bookstore.Models
     {
         public List<BasketLineItem> Items { get; set; } = new List<BasketLineItem>();
 
-        public void AddItem (Book book, int qty)
+        public virtual void AddItem (Book book, int qty)
         {
             BasketLineItem line = Items
                 .Where(x => x.Book.BookId == book.BookId)
@@ -29,7 +29,18 @@ namespace Bookstore.Models
             }
         }
 
-        public double CalculateTotal()
+        public virtual void RemoveItem(Book book)
+        {
+            Items.RemoveAll(x => x.Book.BookId == book.BookId);
+        }
+
+        public virtual void ClearBasket()
+        {
+            Items.Clear();
+        }
+
+
+        public virtual double CalculateTotal()
         {
             double sum = Items.Sum(x => x.Quantity * x.Book.Price);
 
--- Pages/Purchase.cshtml.cs
+++ Pages/Purchase.cshtml.cs
@@ -23,17 +23,17 @@ namespace Bookstore.Pages
         public void OnGet(string returnUrl)
         {
             ReturnUrl = returnUrl ?? "/";
-            basket = HttpContext.Session.GetJson<Basket>("basket") ?? new Basket();
+            //basket = HttpContext.Session.GetJson<Basket>("basket") ?? new Basket();
         }
 
         public IActionResult OnPost(int bookId, string returnUrl)
         {
             Book b = repo.Books.FirstOrDefault(x => x.BookId == bookId);
 
-            basket = HttpContext.Session.GetJson<Basket>("basket") ?? new Basket();
+            //basket = HttpContext.Session.GetJson<Basket>("basket") ?? new Basket();
             basket.AddItem(b, 1);
 
-            HttpContext.Session.SetJson("basket", basket);
+            //HttpContext.Session.SetJson("basket", basket);
 
             return RedirectToPage(new { ReturnUrl = returnUrl });
 
