8e8e1803379251792e8102b19beb29b191b10560
Jane Lee <xo930497@yahoo.com.tw>
2022-03-02 23:21:45 -0700

    checkout controller

---
+++ Controllers/CheckoutController.cs
@@ -0,0 +1,17 @@
+﻿using Bookstore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        public IActionResult Checkout()
+        {
+            return View(new Checkout());
+        }
+    }
+}
