f86ccb9e28675a9c46dd170b729f3185ea44af3f
Jane Lee <xo930497@yahoo.com.tw>
2022-03-03 01:59:36 -0700

    font-awesome & cart

---
+++ Components/PurchaseSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Bookstore.Models;
+
+namespace Bookstore.Components
+{
+    public class PurchaseSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public PurchaseSummaryViewComponent(Basket b)
+        {
+            basket = b;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
