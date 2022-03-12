c0f30419d777c772fadefb5f37faf6fa45c5f570
Jane Lee <xo930497@yahoo.com.tw>
2022-03-11 22:24:16 -0700

    CRUD

--- Models/Book.cs
+++ Models/Book.cs
@@ -13,6 +13,8 @@ namespace Bookstore.Models
         [Key]
         [Required]
         public long BookId { get; set; }
+
+        [Required]
         public string Title { get; set; }
         public string Author { get; set; }
         public string Publisher { get; set; }
